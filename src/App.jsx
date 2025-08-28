31910c6b0c4a3163946d43a4971460ae6af22aac
GABRIEL OLIVEIRA COSTA DA SILVA <gabriel_oc_silva@estudante.sesisenai.org.br>
2025-08-28 11:20:18 -0300

    28/08

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+      
+    <Routes>
+      <Route path="/" element={''} />
+    </Routes>
+
+  </Router>
+  )
+}
+
+export default App
