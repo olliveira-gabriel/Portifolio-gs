f8015bf9df2b4f015b6b0d5993dd8f6bc240982f
Gabriel Oliveira Costa da Silva <GOCSilva@mpsc.mp.br>
2025-11-11 17:20:25 -0300

    11/11

--- src/pages/Humanas.jsx
+++ src/pages/Humanas.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import AtividadeCanva from '../components/AtividadeCanva'
+import AtividadeDocx from '../components/AtividadeDocx'
+import AtividadeImg from '../components/AtividadeImg'
+import ImgOp from '../assets/img-linguagens/operação.png'
 
 function Humanas() {
   return (
@@ -28,29 +31,29 @@ function Humanas() {
             }
             LinkAtv="https://www.canva.com/design/DAGyftycPrQ/zUDTx7Y6_ixdgQ1mNRPe8w/view?embed"
         />
-            <h1 className='bio-texto'>Segunda Trimestre</h1>
             <AtividadeCanva
             TituloAtv="Quiz Imperialismo dos EUA"
             AssuntoAtv={
-            <>
+                <>
                 <span className="prefixo">Assunto:</span>
                 <p className="resto">Imperialismo dos EUA século XIX</p>
             </>
             }
             Competencias={
-            <>
+                <>
                 <span className="prefixo">Habilidades desenvolvidas:</span>
                 <p className="resto">C2 - H8, H10 e H12</p>
             </>
             }
             ObjetivoAtv={
-            <>
+                <>
                 <span className="prefixo">Objetivo da atividade:</span>
                 <p className="resto">O objetivo dessa atividade é promover a leitura e compreensão de um período importante da história dos Estados Unidos, incentivando a pesquisa e a colaboração em grupo. Ao criar um quiz, os alunos aprofundam seu conhecimento sobre temas como a Marcha para o Oeste, o expansionismo, e a Doutrina Monroe.</p>
             </>
             }
             LinkAtv="https://www.canva.com/design/DAGiWwCIfrg/SOPuD5yq_3LN7sHFTi89MQ/view?embed"
         />
+            <h1 className='bio-texto'>Segunda Trimestre</h1>
             <AtividadeCanva
             TituloAtv="Jornal 1ª Guerra"
             AssuntoAtv={
@@ -117,6 +120,58 @@ function Humanas() {
             }
             LinkAtv="https://www.canva.com/design/DAGsnAiuJcA/bMElV1OhWc69e3kODM-R3w/view?embed"
         />
+        <h1 className='bio-texto'>Terceiro Trimestre</h1>
+        <AtividadeDocx
+        TituloAtv=" Dossiê da cientista”"
+        Competencias={
+          <>
+            <span className="prefixo">Habilidades desenvolvidas:</span>
+            <p className="resto">C6 - H39 - H40</p>
+          </>
+        }
+        ObjetivoAtv={
+          <>
+            <span className="prefixo">Objetivo da atividade:</span>
+            <p className="resto">Após a discussão sobre o Massacre de Manguinhos, a atividade consistiu em escolher um dos cientistas perseguidos e produzir um dossiê sobre sua vida e carreira.</p>
+          </>
+        }
+        LinkAtv="https://docs.google.com/document/d/e/2PACX-1vRIl0wpq7CE40aNGHX3uClJhAKIFKocDhBS9o86YHff5f-u9ulKGVczbRZxizwmTAYvqQ0bFFQVvDFx/pub"
+      />
+        <AtividadeDocx
+        TituloAtv="  Nova Capital do Brasil — Aurora do Sul”"
+        Competencias={
+          <>
+            <span className="prefixo">Habilidades desenvolvidas:</span>
+            <p className="resto">C6 - H39 - H40</p>
+          </>
+        }
+        ObjetivoAtv={
+          <>
+            <span className="prefixo">Objetivo da atividade:</span>
+            <p className="resto"> A atividade consistiu em criar um projeto fictício para uma nova capital brasileira chamada Aurora do Sul, planejada para representar modernidade, sustentabilidade e integração regional. </p>
+          </>
+        }
+        LinkAtv="https://docs.google.com/document/d/e/2PACX-1vQMCMlz1AYbNRVg-SM7sIcyQHwHvcluKFJWqelWKgxWws-oITZLMXg8do8Po-G1cIO4jfwD_ofsKLcS/pub"
+      />
+       <AtividadeImg
+        TituloAtv="Operação Brother Sam: O Apoio dos EUA ao Golpe Militar de 1964"
+        ImgAtv={ImgOp}
+        Competencias={
+          <>
+            <span className="prefixo">Habilidades desenvolvidas:</span>
+            <p className="resto">  H6 - H8</p>
+          </>
+        }
+        ObjetivoAtv={
+          <>
+            <span className="prefixo">Objetivo da atividade:</span>
+            <p className="resto"> descrição:  A turma foi dividida em grupos para pesquisar como os Estados Unidos influenciaram o golpe de 1964 no Brasil. Cada grupo estudou um tema específico e produziu um infográfico com imagens, além de uma apresentação oral explicando os principais mecanismos usados pelos EUA, como ações econômicas, midiáticas, militares e de inteligência.</p>
+          </>
+        }
+
+        LinkAtv="https://operacao-brother-sam-o-a-7zto44w.gamma.site/"
+      
+      />
         </Container>
 
   )
