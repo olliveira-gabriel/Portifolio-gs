088dda8a3da56fea1261cab222bf3a3f16a780b1
olliveira-gabriel <gshooter03@gmail.com>
2025-11-16 21:02:52 -0300

    16/11

--- src/components/PortfoliosAntigos.jsx
+++ src/components/PortfoliosAntigos.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import ImgPort from "../assets/img-tecnico/Portifólio.png";
+import ImgPort2 from "../assets/img-tecnico/Port2.png";
+import "../app.css";
 
-function PortfoliosAntigos({ url }) {
+function PortfoliosAntigos(props) {
   return (
-    <div className="iframe-container">
-      <iframe
-        loading="lazy"
-        className="iframe-embed"
-        src={url}
-        allowFullScreen
-      ></iframe>
-    </div>
-  )
+    <Container className="port-antigo">
+      <Row>
+        <Col xs={12} sm={10} md={6} lg={5} className="col-port ">
+          <img src={ImgPort} />
+          <button
+            className="btn-curriculo"
+            onClick={() => window.open(props.LinkAtv, "_blank")}
+          >
+            Ver portfólio
+          </button>
+        </Col>
+
+        <Col  className="col-port">
+          <img src={ImgPort2} />
+          <button
+            className="btn-curriculo"
+            onClick={() => window.open(props.LinkAtv, "_blank")}
+          >
+            Ver portfólio
+          </button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default PortfoliosAntigos
+export default PortfoliosAntigos;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div>
       <Biografia/>
       <h1 className='bio-texto'>Portfólio anteriores</h1>
-      <PortfoliosAntigos url="https://www.canva.com/design/DAGyZoqMY7Y/qtztYNUADpIxCAWs7spW_Q/watch?embed" />
+      <PortfoliosAntigos LinkAtv="https://www.canva.com/design/DAGyZoqMY7Y/qtztYNUADpIxCAWs7spW_Q/watch?embed" />
     </div>
 
   )
